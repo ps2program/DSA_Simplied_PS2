a79a066420a77c277cf5b65208cd83cf4ff10011
ps2program <107313898+ps2program@users.noreply.github.com>
2023-06-16 22:41:34 +0530

    Update and rename 7_CourseScheduleCanFind.js to 7_CourseScheduleCanFindTopologicalSorting.js

--- 1.Graph/7_CourseScheduleCanFindTopologicalSorting.js
+++ 1.Graph/7_CourseScheduleCanFindTopologicalSorting.js
@@ -1,5 +1,4 @@
 function canFinish(numCourses, prerequisites) {
-  // Step 1: Build a directed graph
   const graph = Array.from({ length: numCourses }, () => []);
   const inDegree = Array(numCourses).fill(0);
 
@@ -8,33 +7,40 @@ function canFinish(numCourses, prerequisites) {
     inDegree[course]++;
   }
 
-  // Step 2: Perform topological sorting
   const stack = [];
   const visited = new Set();
+  const visiting = new Set();
 
-  function topologicalSort(course) {
-    visited.add(course);
+  function hasCycle(course) {
+    if (visiting.has(course)) {
+      return true; // Cycle detected
+    }
+
+    if (visited.has(course)) {
+      return false; // Already visited and no cycle
+    }
+
+    visiting.add(course);
     for (const nextCourse of graph[course]) {
-      if (!visited.has(nextCourse)) {
-        topologicalSort(nextCourse);
+      if (hasCycle(nextCourse)) {
+        return true; // Cycle detected
       }
     }
+    visiting.delete(course);
+    visited.add(course);
     stack.push(course);
+    return false;
   }
 
   for (let i = 0; i < numCourses; i++) {
-    if (inDegree[i] === 0 && !visited.has(i)) {
-      topologicalSort(i);
+    if (!visited.has(i) && hasCycle(i)) {
+      return false; // Cycle detected, cannot finish all courses
     }
   }
 
-  // Step 3: Check if there is a valid course order
-  if (stack.length !== numCourses) {
-    return false; // Cannot finish all courses
-  }
-
-  return true; // Can finish all courses
+  return true; // No cycles, can finish all courses
 }
+
 
 // Example usage:
 const numCourses = 4;
@@ -42,3 +48,5 @@ const prerequisites = [[1, 0], [2, 0], [3, 1], [3, 2]];
 
 const canFinishAll = canFinish(numCourses, prerequisites);
 console.log('Can finish all courses:', canFinishAll);
+
+

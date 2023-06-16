ce16a60d1f5f1539cd85ba3eaed6e816534dded9
ps2program <107313898+ps2program@users.noreply.github.com>
2023-06-17 01:17:00 +0530

    Update 7_CourseScheduleIndegreeTopologicalSorting.js

--- 1.Graph/7_CourseScheduleIndegreeTopologicalSorting.js
+++ 1.Graph/7_CourseScheduleIndegreeTopologicalSorting.js
@@ -50,3 +50,25 @@ const canFinishAll = canFinish(numCourses, prerequisites);
 console.log('Can finish all courses:', canFinishAll);
 
 
+/*
+
+there is another approach to solve this problem using the concept of indegree and topological sorting.
+
+Here's the alternative approach:
+
+Create an adjacency list representation of the graph using the prerequisites array.
+Create an array called indegree of size numCourses and initialize all its elements to 0.
+Iterate through the prerequisites array and increment the indegree of each course based on its prerequisite relationship.
+Create a queue and enqueue all the courses with an indegree of 0.
+Initialize a variable called count to keep track of the number of courses that can be finished.
+While the queue is not empty, perform the following steps:
+  Dequeue a course from the queue.
+  Increment count by 1 to indicate that a course has been finished.
+  Iterate through the neighbors of the dequeued course in the adjacency list.
+  Decrement the indegree of each neighbor.
+  If the indegree of a neighbor becomes 0, enqueue it.
+Finally, check if count is equal to numCourses. If they are equal, it means all courses can be finished, so return true. Otherwise, return false.
+Here's the implementation in JavaScript:
+
+*/
+
